deb1925508635e3a98856c3d5a39ae7c0e656d6b
Sagar <sagar20032610@gmail.com>
2025-01-13 22:30:01 +0530

    commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// script.js
+function adjustPageWidth() {
+    const width = window.innerWidth;
+    const container = document.querySelector('.container');
+  
+    if (width > 992 && width <= 1600) {
+      container.style.transform = 'scale(0.9)';
+      container.style.transformOrigin = 'top center';
+    } else if (width > 700 && width <= 767) {
+      container.style.transform = 'scale(0.8)';
+      container.style.transformOrigin = 'top center';
+    } else if (width > 600 && width <= 700) {
+      container.style.transform = 'scale(0.75)';
+      container.style.transformOrigin = 'top center';
+    } else if (width <= 600) {
+      container.style.transform = 'scale(0.5)';
+      container.style.transformOrigin = 'top center';
+    } else {
+      container.style.transform = 'scale(1)';
+      container.style.transformOrigin = 'top center';
+    }
+  }
+  
+  // Adjust page width on load and resize
+  window.addEventListener('load', adjustPageWidth);
+  window.addEventListener('resize', adjustPageWidth);
+  
+  // Toggle the left menu
+  document.getElementById('toggleMenu').addEventListener('click', () => {
+    const leftMenu = document.querySelector('.left-menu');
+    leftMenu.classList.toggle('collapsed');
+  });
+  
